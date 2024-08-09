a65e94a9b8d91077b7c5037907f96f0d490c4cc6
Nadia-Kadri <nadiagrace96@gmail.com>
2024-08-08 20:31:58 -0400

    Add current streak and best streak functionality to Stats component, fix alignment issues

--- client/src/pages/Home/Dashboard/MonthView/Stats.jsx
+++ client/src/pages/Home/Dashboard/MonthView/Stats.jsx
@@ -4,16 +4,16 @@ import EventAvailableTwoToneIcon from '@mui/icons-material/EventAvailableTwoTone
 import AutoAwesomeMotionTwoToneIcon from '@mui/icons-material/AutoAwesomeMotionTwoTone';
 import TaskAltIcon from '@mui/icons-material/TaskAlt';
 import IconWithText from './IconWithText';
-import { parseISO, format } from 'date-fns';
+import { parseISO, format, differenceInDays } from 'date-fns';
 
 function Stats({ selectedHabit, selectedDate }) {
   const sortedLogs = selectedHabit ? selectedHabit.logs.map(log => parseISO(log.log_created_at)).sort((a, b) => a - b) : [];
-  // const today = new Date();
+  const today = new Date();
 
   const daysInMonth = selectedHabit ? calcDaysInMonth() : 0;
   const totalDays = selectedHabit ? selectedHabit.logs.length : 0;
-  const currentStreak = 0;
-  const bestStreak = 0;
+  const currentStreak = selectedHabit ? calcCurrentStreak() : 0;
+  const bestStreak = selectedHabit ? calcBestStreak() : 0;
 
   function calcDaysInMonth() {
     let daysInMonth = 0;
@@ -25,23 +25,43 @@ function Stats({ selectedHabit, selectedDate }) {
     return daysInMonth;
   }
 
-  // function calcCurrentStreak() {
-  //   const todayIndex = sortedLogs.find((log) => log.toDateString() === today.toDateString());
-  //   if(!todayIndex) return 0;
-  //   let streak = 1;
-  //   for (let i = sortedLogs.indexOf(todayIndex); i > 0; i--) {
-  //     if (sortedLogs[i].getTime() - sortedLogs[i - 1].getTime() === 86400000) {
-  //       streak++;
-  //     } else {
-  //       break;
-  //     }
-  //   }
-  //   return streak;
-  // }
+  function calcCurrentStreak() {
+    const yesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1);
+    const yesterdayIndex = sortedLogs.findIndex((log) => log.toDateString() === yesterday.toDateString());
+    const todayIndex = sortedLogs.findIndex((log) => log.toDateString() === today.toDateString());
+  
+    if(todayIndex < 0 && yesterdayIndex < 0) return 0;
 
-  // function calcBestStreak() {
+    let streak = todayIndex >= 0 ? 1 : 0;
+    streak += yesterdayIndex >= 0 ? 1 : 0;
 
-  // }
+    for(let i = yesterdayIndex; i > 0; i--) {
+      if (differenceInDays(sortedLogs[i], sortedLogs[i - 1]) === 1) {
+        streak++;
+      } else {
+        break;
+      }
+    }
+    return streak;
+  }
+
+  function calcBestStreak() {
+    if(sortedLogs.length === 0) return 0;
+
+    let bestStreak = 1;
+    let currentStreak = 1;
+    
+    for(let i = 1; i < sortedLogs.length; i++) {
+      if(differenceInDays(sortedLogs[i], sortedLogs[i - 1]) === 1) {
+        currentStreak++;
+      } else {
+        bestStreak = Math.max(bestStreak, currentStreak);
+        currentStreak = 1;
+      }
+    }
+
+    return Math.max(bestStreak, currentStreak);
+  }
   
   return (
     <>
