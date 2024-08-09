cc10f3ded44d3ea9a18a7ec00a96e91f85479906
Nadia-Kadri <nadiagrace96@gmail.com>
2024-08-09 13:06:55 -0400

    Add and style home page image and text, add and style text in day view when the user has no habits

--- client/src/pages/Home/Dashboard/DayView/DayView.jsx
+++ client/src/pages/Home/Dashboard/DayView/DayView.jsx
@@ -74,43 +74,46 @@ function DayView({ habits, getUserHabits }) {
         <Typography>{format(selectedDate, "EEEE, MMM do")}</Typography>
         <IconButton onClick={handleNextClick}><NavigateNextIcon /></IconButton>
       </Box>
-      {habits.length === 0 ? <Typography>No current habits</Typography> : (
+
         <Box sx={{ height: '364px', overflow: 'auto', px: 3 }}>
-          {habits.map(habit => {
-            const completedLog = habit.logs.find(log => log.log_created_at === format(selectedDate, 'yyyy-MM-dd'));
-            const habitStartDate = new Date(habit.created_at);
-            const habitEndDate = new Date(habit.expired_at);
-            if(habitStartDate.getTime() <= selectedDate.getTime() && habitEndDate.getTime() >= selectedDate.getTime()) {
-              return (
-                <Box
-                  key={habit.id}
-                  sx={{ 
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    borderBottom: `2px solid ${habit.color}`,
-                    mb: '14px',
-                    overflow: 'auto',
-                    opacity: selectedDate > today ? 0.5 : 1, 
-                    pointerEvents: selectedDate > today ? 'none' : 'auto' 
-                  }}
-                >
-                  <Box>
-                    <Typography>{habit.icon} {habit.name}</Typography>
-                    <Typography sx={{ fontSize: '.8rem', paddingLeft: '14px' }}>{habit.amount} {habit.unit}</Typography>
+          {habits.length === 0 ? (
+            <Typography variant='body2' align='center'>It look&#39;s like you dont have any habits yet. Start tracking your progress by creating a new habit. Click the button below to get started!</Typography>
+          ) : (
+            habits.map(habit => {
+              const completedLog = habit.logs.find(log => log.log_created_at === format(selectedDate, 'yyyy-MM-dd'));
+              const habitStartDate = new Date(habit.created_at);
+              const habitEndDate = new Date(habit.expired_at);
+              if(habitStartDate.getTime() <= selectedDate.getTime() && habitEndDate.getTime() >= selectedDate.getTime()) {
+                return (
+                  <Box
+                    key={habit.id}
+                    sx={{ 
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      borderBottom: `2px solid ${habit.color}`,
+                      mb: '14px',
+                      overflow: 'auto',
+                      opacity: selectedDate > today ? 0.5 : 1, 
+                      pointerEvents: selectedDate > today ? 'none' : 'auto' 
+                    }}
+                  >
+                    <Box>
+                      <Typography>{habit.icon} {habit.name}</Typography>
+                      <Typography sx={{ fontSize: '.8rem', paddingLeft: '14px' }}>{habit.amount} {habit.unit}</Typography>
+                    </Box>
+                    <Checkbox
+                      id={`checkbox-${habit.id}`}
+                      name={`checkbox-${habit.id}`}
+                      checked={completedLog ? true : false}
+                      onChange={completedLog ? () => unCompleteHabit(completedLog.log_id) : () => completeHabit(habit.id, format(selectedDate, 'yyyy-MM-dd'))}
+                      sx={{ padding: '0', color: habit.color, '& .MuiSvgIcon-root': { color: habit.color} }}
+                    />
                   </Box>
-                  <Checkbox
-                    id={`checkbox-${habit.id}`}
-                    name={`checkbox-${habit.id}`}
-                    checked={completedLog ? true : false}
-                    onChange={completedLog ? () => unCompleteHabit(completedLog.log_id) : () => completeHabit(habit.id, format(selectedDate, 'yyyy-MM-dd'))}
-                    sx={{ padding: '0', color: habit.color, '& .MuiSvgIcon-root': { color: habit.color} }}
-                  />
-                </Box>
-              )
-            }
-          })}
+                )
+              }
+            })
+          )}
         </Box>
-      )}
       <Divider />
       <CreateHabitModal getUserHabits={getUserHabits} setSelectedDate={setSelectedDate}/>
     </Paper>
--- client/src/pages/Home/Welcome.jsx
+++ client/src/pages/Home/Welcome.jsx
@@ -3,15 +3,17 @@ import { Container, Box, Button, Typography } from '@mui/material';
 
 function Welcome() {
   return ( 
-    <>
-    <Container maxWidth='md' sx={{ my: 4 }}>
-      <Box>
-        <Typography component='h1' variant='h4'>Welcome to Progresspal!</Typography>
-        <Button component={Link} to="/login" variant="contained" color="primary" sx={{ marginRight: '1rem' }}>Login</Button>
-        <Button component={Link} to="/register" variant="outlined" color="primary">Register</Button>
-      </Box>
-    </Container>
-    </>
+    <Box component='main'>
+      <Container>
+        <img src='../../public/images/progresspal-4.png' alt='Welcome' style={{ height: '350px' }} />
+        <Box>
+          <Typography component='h1' variant='h2'>Let&#39;s Build Better Habits</Typography>
+          <Typography component='h2' variant='h6'>A platform for tracking and achieving your goals</Typography>
+          <Button component={Link} to="/login" variant="contained" color="primary" sx={{ marginRight: '1rem' }}>Login</Button>
+          <Button component={Link} to="/register" variant="outlined" color="primary">Register</Button>
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
